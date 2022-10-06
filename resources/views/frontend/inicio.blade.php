87c6c0a131cd914e9757b9427cd069116712e165
Mersmith <mersmith14@gmail.com>
2022-10-06 11:21:24 -0500

    Creando Layouts de Administrador y Frontend

---
+++ resources/views/frontend/inicio.blade.php
@@ -0,0 +1,7 @@
+<x-frontend-layout>
+    @section('tituloPagina', 'Inicio')
+    <h1>Pagina Inicio</h1>
+    <div>Slider</div>
+    <div>Categorias</div>
+</x-frontend-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\InicioController;
 
 require_once __DIR__ . '/fortify.php';
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', InicioController::class)->name('inicio');
 
 Route::middleware([
     'auth:sanctum',
