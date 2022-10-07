691d4cdcaaf262af8ca9c8c2d93b38113871d66d
Mersmith <mersmith14@gmail.com>
2022-10-07 18:03:22 -0500

    Avance Menu Principal

---
+++ resources/views/layouts/frontend/componentes/css.blade.php
@@ -0,0 +1,8 @@
+<!-- Fonts -->
+
+{{-- Fontawesome --}}
+<link rel="stylesheet" href="{{ asset('vendor/fontawesome-free-6.1.2-web/css/all.min.css') }}">
+
+<!-- Styles -->
+<link rel="stylesheet" href="{{ asset('Inicio/css/menu-principal.css') }}">
+<link rel="stylesheet" href="{{ asset('Inicio/css/slider-principal.css') }}">
