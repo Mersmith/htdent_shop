3c644c10e7c4e51b869d5c215c1d99cf0a76bac5
Mersmith <mersmith14@gmail.com>
2022-10-17 12:36:35 -0500

    Pie pagina

--- resources/views/layouts/frontend/componentes/css.blade.php
+++ resources/views/layouts/frontend/componentes/css.blade.php
@@ -8,3 +8,7 @@
 <link rel="stylesheet" href="{{ asset('frontend/inicio/css/slider-principal.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/inicio/css/slider-producto.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/inicio/css/slider-testimonio.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/inicio/css/pie-pagina.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/inicio/css/llamada-accion.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/inicio/css/slider-iconos.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/inicio/css/slider-metodo-pago.css') }}">
